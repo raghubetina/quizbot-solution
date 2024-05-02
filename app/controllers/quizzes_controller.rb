329b5dbc2219d4c0223094ea50c6c2c16073c5a1
Raghu Betina <Raghu.Betina@chicagobooth.edu>
2024-05-02 19:40:00 +0000

    Can see list of quizzes

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,5 +1,6 @@
 class QuizzesController < ApplicationController
   def index
+    @list_of_quizzes = Quiz.all.order({ :created_at => :desc })
 
     render({ :template => "quiz_templates/list" })
   end
--- lib/tasks/dev.rake
+++ lib/tasks/dev.rake
@@ -1,3 +1,4 @@
 desc "Fill the database tables with some sample data"
 task({ :sample_data => :environment }) do
+  pp "howdy"
 end
