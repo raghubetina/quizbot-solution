a8b7abb60ef2e7322f2b7fb9eec0b82d2e55ba8a
Raghu Betina <Raghu.Betina@chicagobooth.edu>
2024-05-02 19:45:44 +0000

    Users can create quizzes

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -4,4 +4,14 @@ class QuizzesController < ApplicationController
 
     render({ :template => "quiz_templates/list" })
   end
+
+  def create
+    # {"query_topic"=>"tennis"}
+
+    q = Quiz.new
+    q.topic = params.fetch("query_topic")
+    q.save
+
+    redirect_to "/quizzes"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # /quizzes - list of all quizzes
   get("/quizzes", { :controller => "quizzes", :action => "index" })
 
+  post("/insert_quiz", { :controller => "quizzes", :action => "create" })
+
   # /quizzes/42 - details of one quiz
 end
